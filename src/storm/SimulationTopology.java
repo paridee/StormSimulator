1e567a552476166d1fcc44dabb67260d0024a877
paride <paride.casulli@gmail.com>
2016-06-27 16:41:54 +0200

    added local mode

--- src/storm/SimulationTopology.java
+++ src/storm/SimulationTopology.java
@@ -18,12 +18,18 @@ public class SimulationTopology {
     	builder.setSpout("spout", new ConfigurationSpout(), 1);
     	builder.setBolt("firststage", new SimulationBolt(), 32).shuffleGrouping("spout");
     	builder.setBolt("finalstage", new SimulationBolt(), 1).shuffleGrouping("firststage");
-    	try {
-			StormSubmitter.submitTopology(args[0]+"", conf, builder.createTopology());
-		} catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	if(args.length==0){
+  	      LocalCluster cluster = new LocalCluster();
+  	      cluster.submitTopology("localSimulator", conf, builder.createTopology());
+    	}
+    	else{
+	    	try {
+				StormSubmitter.submitTopology(args[0]+"", conf, builder.createTopology());
+			} catch (AlreadyAliveException | InvalidTopologyException | AuthorizationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
     }
  	//builder.setSpout("spout", new ConfigurationSpout(5000,intervals), 1);
 	//    builder.setBolt("firststage", new IntermediateWorker(5000), 32).shuffleGrouping("spout");
