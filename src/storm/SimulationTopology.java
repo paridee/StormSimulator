a907866f3113b76cce11dfcc5b2a5bf3e4c3cde2
paride <paride.casulli@gmail.com>
2016-06-28 12:42:06 +0200

    fixed task number

--- src/storm/SimulationTopology.java
+++ src/storm/SimulationTopology.java
@@ -20,7 +20,7 @@ public class SimulationTopology {
     	
     	conf.setDebug(true);
     	builder.setSpout("spout", new ConfigurationSpout(32), 1);
-    	builder.setBolt("firststage", new SimulationBolt(), 1).shuffleGrouping("spout");
+    	builder.setBolt("firststage", new SimulationBolt(), 32).shuffleGrouping("spout");
     	builder.setBolt("finalstage", new ResultsBolt(), 1).shuffleGrouping("firststage");
     	if(args.length==0){
     		conf.setMaxTaskParallelism(3);
